b10c630073838324bbb06ff84bfd42ea0c565c9a
galen <1149501789@qq.com>
2020-12-29 10:33:45 +0800

    完成了手机版的适配, 能连贯画线, 没有间断

--- javascript.js
+++ javascript.js
@@ -2,27 +2,52 @@ var canvas = document.getElementById("canvas");
 canvas.width = document.documentElement.clientWidth
 canvas.height = document.documentElement.clientHeight
 var ctx = canvas.getContext("2d");
+ctx.beginPath()
+ctx.lineWidth = 5
+ctx.lineCap = 'round'
 let isPainting = false
+let isTouchDevice = 'ontouchstart' in document.documentElement;
+let pointPre = {x:0,y:0}
 
-canvas.onmousedown =(e)=>{
-    isPainting = true
-    draw(e.clientX, e.clientY)
-}
-canvas.onmouseup =()=>{
-    isPainting = false
-}
-canvas.onmousemove = (e) =>{
-    if(isPainting){
-        var x = e.clientX
-        var y = e.clientY
-        draw(x, y)
+if(isTouchDevice){
+
+    canvas.ontouchstart = ev => {
+        pointPre.x=ev.touches[0].clientX
+        pointPre.y=ev.touches[0].clientY
+        // draw(ev.touches[0].clientX, ev.touches[0].clientY)
     }
 
+    canvas.ontouchmove = ev => {
+        ctx.moveTo(pointPre.x, pointPre.y)
+        pointPre.x=ev.touches[0].clientX
+        pointPre.y=ev.touches[0].clientY
+        ctx.lineTo(pointPre.x, pointPre.y)
+        ctx.stroke()
+    }
+
+}else{
+    canvas.onmousedown =(e)=>{
+        isPainting = true
+        pointPre.x=e.clientX
+        pointPre.y=e.clientY
+        // draw(e.clientX, e.clientY)
+    }
+    canvas.onmouseup =()=>{
+        isPainting = false
+    }
+    canvas.onmousemove = (e) =>{
+        if(isPainting){
+            ctx.moveTo(pointPre.x, pointPre.y)
+            pointPre.x=e.clientX
+            pointPre.y=e.clientY
+            ctx.lineTo(pointPre.x, pointPre.y)
+            ctx.stroke()
+        }
+
+    }
 }
 var draw = (x, y)=>{
-    // ctx.fillStyle = "rgb(200,0,0)";
-    // ctx.fillRect (x-3, y-3, 6, 6);
     ctx.beginPath();
-    ctx.arc(x, y, 3, 0, 2 * Math.PI);
+    ctx.arc(x, y, 10, 0, 2 * Math.PI);
     ctx.fill();
 }
